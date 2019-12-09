67dc2f89490876935e63113c45223a4d6ddd3edc
Alton Lee <altonlee@hawaii.edu>
2019-12-09 02:00:39 -1000

    edit modal functionality

--- scheduler.js
+++ scheduler.js
@@ -25,11 +25,12 @@ function addCourse() {
 
     // Check form results
     for (i = 0; i < form.length; i++) {
-        if (form[i].name === "weekday") {
+        if (form[i].name === "add-weekday") {
             days.push(form[i].value);
             course["weekdays"] = days;
         } else {
-            course[form[i].name] = form[i].value;
+            var name = form[i].name.replace('add-', "");
+            course[name] = form[i].value;
         }
     }
 
@@ -48,6 +49,55 @@ function addCourse() {
         document.getElementById('add-course').reset();
         console.log(schedule);
         toggle('#add-modal');        
+    }
+}
+
+
+// Grabs form data and edits Course's values.
+// Does not take any parameters. 
+function editCourse() {
+    console.log("saved!");
+    toggle('#edit-modal');
+}
+
+// Pops course from schedule[]. 
+// Does not take any parameters.
+function deleteCourse() {
+    console.log("delete!");
+    toggle('#edit-modal');
+}
+
+// Autofills Edit Course form with the selected course's details.
+// Does not take any parameters.
+function autofill() {
+    var selected = document.getElementById("edit-selection").value;
+    
+    if (selected === "none") {
+        document.getElementById("edit-course").style.display = "none";
+    } else {
+        var course = schedule[selected];
+        document.getElementById("edit-course").style.display = "block";
+        
+        document.getElementById("edit-courseName").value = course.courseName;
+        document.getElementById("edit-color").value = course.color;
+        document.getElementById("edit-courseType").value = course.courseType;
+        document.getElementById("edit-instructor").value = course.instructor;
+        document.getElementById("edit-location").value = course.location;
+        document.getElementById("edit-timeStart").value = course.timeStart;
+        document.getElementById("edit-timeEnd").value = course.timeEnd;
+    }
+}
+
+
+// Populates the Dropdown list with the current user's courses stored in 
+// schedule[].
+// Does not take any parameters. 
+function editSelection() {
+    var select = document.getElementById("edit-selection");
+    select.options[0] = new Option('- - -', 'none', true, true);
+    
+    for (var i = 0; i < schedule.length; i++) {
+        select.options[select.options.length] = new Option(schedule[i].courseName, i, false, false);
     }
 }
 
